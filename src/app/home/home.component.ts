05e6b170cf47482cac8e5e2c69716c83562471a0
balaji <balajikvirat@gmail.com>
2021-07-24 14:41:02 +0530

    final

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HostListener } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  value = 1;
   constructor() { }
 
   ngOnInit(): void {
+    this.slide();
   }
+  @HostListener('window:scroll', ['$event']) 
+  scrollHandler(event:any) {
+    console.log(window.pageYOffset);
 
+    if (window.pageYOffset > 585) {
+      var header = document.getElementById("headerContent")!;
+      header.setAttribute(
+        "style", "position: fixed; background-color: white; color: black; box-shadow: 0px 4px 20px 0px #dccaca;");
+    }else if (window.pageYOffset < 585) {
+      var header = document.getElementById("headerContent")!;
+      header.setAttribute(
+        "style", "position: relative;");
+    }
+    
+  }
+ 
+  slide(){
+    var header = document.getElementById("header-img")!;
+    var images = header.getElementsByTagName("img");
+    for (let index = 0; index < images.length; index++) {
+      images[index].style.display = "none";
+    }
+    this.value++;
+    if (this.value > images.length) {
+      this.value = 1;
+    }
+    
+    images[this.value -1].style.display = "block";
+    setTimeout(() => {
+      this.slide();
+    }, 3000);
+  }
 }
